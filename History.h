55d52c45b80bbf2d15a84bcdb877739d6e57af9f
Tania Tkachyk <120555490+taniaTkachyk@users.noreply.github.com>
2022-12-22 19:58:58 +0200

    Create History.h

---
+++ History.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "./Parcel.h"
+
+
+class History{
+    int parcels_number;
+    Parcel *parcels_history;
+public:
+    History();
+    History(const History &history);
+    History(int number, Parcel *parcels);
+    ~History();
+
+
+    int get_parcels_number ()const;
+    Parcel *get_parcels_history ()const;
+    int set_parcels_number(int n);
+    Parcel* set_parcels_number(Parcel *new_parcels_history);
+
+    void print();
+
+    void update();
+    Parcel& operator [] (int);
+
+};
